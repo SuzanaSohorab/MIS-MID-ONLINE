8ad7ed2061f2d607d8af94741f5862b4df468072
SuzanaSohorab <suzanasohorab5@gmail.com>
2025-03-22 23:17:01 +0600

    7th commit

---
+++ script.js/button.js
@@ -0,0 +1,18 @@
+document.getElementById("getCoffee").addEventListener('click', 
+    function(){
+        //console.log("button clicked")
+        window.location.href= "index2.html";
+
+})
+document.getElementById("contactBtn").addEventListener('click', 
+    function(){
+        //console.log("button clicked")
+        window.location.href= "index3.html";
+
+})
+document.getElementById("HomePage").addEventListener('click', 
+    function(){
+        console.log("button clicked")
+        window.location.href= "index1.html";
+
+})
